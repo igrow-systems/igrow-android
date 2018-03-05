e773e920832a40e07ea2a4c2fbec62069357a0ae
Jeremy Reeve <jeremy@igrow-systems.com>
2018-03-06 12:21:56 +1300

    Add utils to swap fragments in fragmentManager

---
+++ app/src/main/java/com/igrow/android/util/ActivityUtils.java
@@ -0,0 +1,41 @@
+package com.igrow.android.util;
+
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * This provides methods to help Activities load their UI.
+ */
+public class ActivityUtils {
+
+    /**
+     * The {@code fragment} is added to the container view with id {@code frameId}. The operation is
+     * performed by the {@code fragmentManager}.
+     */
+    public static void replaceFragmentInActivity(@NonNull FragmentManager fragmentManager,
+                                                 @NonNull Fragment fragment, int frameId) {
+        checkNotNull(fragmentManager);
+        checkNotNull(fragment);
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(frameId, fragment);
+        transaction.commit();
+    }
+
+    /**
+     * The {@code fragment} is added to the container view with id {@code frameId}. The operation is
+     * performed by the {@code fragmentManager}.
+     */
+    public static void replaceFragmentInActivity(@NonNull FragmentManager fragmentManager,
+                                                 @NonNull Fragment fragment, String tag) {
+        checkNotNull(fragmentManager);
+        checkNotNull(fragment);
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.add(fragment, tag);
+        transaction.commit();
+    }
+}
+
