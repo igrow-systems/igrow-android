8ac8f3106e005dea1a414a967751a47fa9cf2e61
Jeremy Reeve <jeremy@igrow-systems.com>
2018-11-10 15:35:17 +1300

    Add unit tests for the scan proxy - revisit this as the proxies don't have any function except to forward requests to the platform implementations.

---
+++ app/src/test/java/com/igrow/android/bluetooth/BluetoothLeScanL18ProxyTest.java
@@ -0,0 +1,16 @@
+package com.igrow.android.bluetooth;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BluetoothLeScanL18ProxyTest {
+
+    @Test
+    public void startLeScan() {
+    }
+
+    @Test
+    public void stopLeScan() {
+    }
+}
